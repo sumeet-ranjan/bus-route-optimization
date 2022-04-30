ba1de6421f136897e6d4d8798edfec9ff8e92988
sumeet-ranjan <70603282+sumeet-ranjan@users.noreply.github.com>
2022-04-30 10:28:43 +0530

    Add files via upload

---
+++ simulation.py
@@ -0,0 +1,196 @@
+import numpy as np
+
+
+### variable P read from AMPL ###
+
+P = "0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0"
+a= P.split()
+
+n = 5
+R = 3
+
+
+p = np.zeros((n,n,n,n,R))
+
+count = 0 
+for o in range(n):
+	for d in range(n):
+		for i in range(n):
+			for j in range(n):
+				for r in range(R):
+					p[o][d][i][j][r] = a[count]
+					count+=1
+
+
+### variable X read from AMPL ###
+
+m = "0 0 0 0 0 0 0 1 0 1 0 0 0 0 1 0 0 1 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0"
+
+q = m.split()
+
+x = np.zeros((n,n,R))
+
+count = 0 
+
+for i in range(n):
+	for j in range(n):
+		for r in range(R):
+			x[i][j][r] = q[count]
+			#if a[count] == 1:
+				#print(count)
+			count+=1
+
+### variable mean demand read from AMPL ###
+a = "0 50 20 11 24 17 0 3 21 27 1 8 0 31 32 6 8 3 0 9 15 18 19 5 0"
+
+d=a.split();
+
+m_demand = np.zeros((n,n));
+
+count = 0 
+for i in range(n):
+	for j in range(n):
+		m_demand[i][j] = d[count]
+		count+=1
+
+
+### sampling demand from gaussian distribution ###
+
+def sample(deviation=5):
+	demand = np.random.normal(m_demand,deviation,(n,n))
+
+	demand = np.multiply(demand,demand>0)	# convert -ve demand as zero
+
+	for i in range(n):
+		demand[i,i] = 0  					# demand from i -> i = 0
+
+	D = np.around(demand)					# integer demand only
+
+	return demand
+
+### intial and final bus route position ###
+
+e = np.array([1,2,3])
+f = np.array([3,4,5]) 
+
+cap= 50									# bus capacity
+
+m = np.ones((n,n))						# fraction of passengers(o,d) who coud use the bus 
+
+for i in range(n):
+	m[i,i]=0 							# fraction of passengers(i -> i) using bus = 0
+
+def func(D):
+
+	### for bus route r ###
+	for r in range(R):
+		a=[]
+		i = e[r]-1
+		a.append(i)
+
+		### form array of bus route ###
+		for k in range(100):
+			j = np.argmax(x[i,:,r])
+			a.append(j)
+			if j == f[r]-1:
+				break
+			i = j
+
+		for th,i in enumerate(a[:-1]):
+			already_on_bus = 0
+			for o in range(n):
+				if i != o:
+					for d in range(n):
+						already_on_bus+=D[o,d]*p[o,d,i,a[th+1],r]*m[o,d]
+			if already_on_bus > cap:
+				temp = already_on_bus
+				already_on_bus=cap
+				for o in range(n):
+					if i != o:
+						for d in range(n):
+							if p[o,d,i,a[th+1],r] ==1:
+								m[o,d] = min(m[o,d],cap/temp)
+
+			incoming = 0
+			for d in range(n):
+				incoming+=D[i,d]*p[i,d,i,a[th+1],r]
+				
+			for d in range(n):
+				if p[i,d,i,a[th+1],r]==1:
+					m[i,d] = min(1,(cap-already_on_bus)/(incoming+0.001))
+
+		### reverse bus ###
+
+		a = np.flip(a)
+
+		for th,i in enumerate(a[:-1]):
+			already_on_bus = 0
+			for o in range(n):
+				if i != o:
+					for d in range(n):
+						already_on_bus+=D[o,d]*p[o,d,i,a[th+1],r]*m[o,d]
+			if already_on_bus > cap:
+				temp = already_on_bus
+				already_on_bus=cap
+				for o in range(n):
+					if i != o:
+						for d in range(n):
+							if p[o,d,i,a[th+1],r] ==1:
+								m[o,d] = min(m[o,d],cap/temp)
+
+			incoming = 0
+			for d in range(n):
+				incoming+=D[i,d]*p[i,d,i,a[th+1],r]
+				
+			for d in range(n):
+				if p[i,d,i,a[th+1],r]==1:
+					m[i,d] = min(1,(cap-already_on_bus)/(incoming+0.001))
+
+
+for i in range(n):
+	m[i,i]=0 							# fraction of passengers(i -> i) using bus = 0
+
+#print(m)
+
+
+### printing passengers on bus for all routes i -> j
+
+y = "no"
+if y == "yes":
+	for i in range(n):
+		for j in range(n):
+			for r in range(R):
+				u = 0;
+				for o in range(n):
+					for d in range(n):
+						u+=int(D[o,d]*p[o,d,i,j,r]*m[o,d])
+				print(u)
+
+### hundred sampling and simulation ###
+
+count = 0
+average = 0
+deviation = 2.5							# 68 percent of sampling lies between (mean-dev, mean+dev)
+
+for i in range(100):
+	count+=1
+	D = sample(deviation)
+	func(D)
+
+
+	total = np.sum(D)
+	served = np.multiply(D,m)
+	served = np.around(served)
+	#print(np.sum(served)/total)
+	average += np.sum(served)/total
+
+print(f"average efficiency with standard deviation {deviation} is : {average/count}")
+
+
+#print(served/total)
+
+
+#print(p[1,0,:,:,2])
+#print(p[1,2,:,:,0])
+#print(p[1,3,:,:,0])
+#print()
